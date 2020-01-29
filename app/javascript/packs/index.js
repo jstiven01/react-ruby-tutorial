3d1f5f5d2a1fd39f01e8fad0e8f4f7e3fdb5d2d1
jstiven01 <stiven10@gmail.com>
2020-01-28 20:06:33 -0500

    Adding bootstrap to index.js and creatin Structure React Components

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,10 +4,11 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
 import PropTypes from 'prop-types'
 
 const Hello = props => (
-  <div>Hello {props.name}!</div>
+  <div className='container'>Hello {props.name}!</div>
 )
 
 Hello.defaultProps = {
