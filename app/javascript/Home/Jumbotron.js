737bec47ccf345bafaecd56b4cab664f8b205414
jstiven01 <stiven10@gmail.com>
2020-01-29 11:52:59 -0500

    styling jumbotron using yarn add styled-components

--- app/javascript/Home/Jumbotron.js
+++ app/javascript/Home/Jumbotron.js
@@ -1,17 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.section`
+ background-color: red;
+ color: white;
+ min-height: 550px;
+ padding: 100px 0;
+`
+
+const H1 = styled.h1`
+font-family: Courier;
+font-weight: bold;
+line-height: 52px;
+font-size: 40px;
+color: white;
+`
+
+const DescP = styled.p`
+font-size: 18px;
+`
+
+const Button = styled.a`
+        display: inline-block;
+        text-decoration: none;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 0;
+        background: #fff !important;
+        color: #333 !important;
+        padding: 10px 20px;
+        font-size: 18px;
+        box-shadow: 0px 0px 0px 3px #473228,
+            -6px 6px #ef5f17,
+            -6px 6px 0px 3px #473228;
+         `
 
 const Jumbotron = () => {
 
     return (
-        <section className="home-section--1">
+        <Section className="home-section--1">
             <div className="container">
                 <div className="row">
                     <div className="col col-sm-12 col-md-5">
                         <div className="pt-4 mt-4">
-                            <h1>React for Rails Developers</h1>
-                            <p>Supercharge your Ruby on Rails Apps with React.js</p>
+                            <H1>React for Rails Developers</H1>
+                            <DescP>Supercharge your Ruby on Rails Apps with React.js</DescP>
                             <div className="cta-wrapper">
-                                <button className="btn fancy-btn">Get Started</button>
+                                <Button className="btn fancy-btn">Get Started</Button>
                             </div>
                         </div>
                     </div>
@@ -23,7 +58,7 @@ const Jumbotron = () => {
                     </div>
                 </div>
             </div>
-        </section>
+        </Section>
     )
 
 }
